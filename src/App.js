401b531060396e2deafcb82cb17e74879fee3378
lie <1151109616@qq.com>
2022-07-16 13:10:30 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import AppHeader from "./components/app-footer";
+import AppFooter from "./components/app-header";
+import routes from "./router";
+import { Provider } from "react-redux";
+// import store from "./store";
+
+// import HYMain from './pages/main';
+
+function App() {
+  return (
+    <h1>app</h1>
+    // <Provider store={store}>
+    // {/* <HYMain/> */}
+
+    // </Provider>
+  );
+}
+
+export default App;
